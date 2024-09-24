ac704f500d24b3e302d2ae92a20257c6a9bb418f
Agustina444 <alanosa4@gmail.com>
2024-09-23 21:30:39 -0300

    pagina_sedes

---
+++ sedes.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Sedes </title>
+	<link rel="stylesheet" type="text/css" href="formularios.css">
+	<link href="https://fonts.googleapis.com/css2?family=Bakbak+One&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<header>
+		<div class="menu">	
+			<nav>
+				<ul>
+					<li><a href="pagina.html">INICIO</a></li>
+				</ul>
+			</nav>	
+		</div>
+
+		   	<section class="contenedor">
+		   		
+		   		<div class="sedes">
+	                <div class="img_sedes">
+	         			<p> ACA VA UNA IMAGEN </p>
+	            	</div>  
+
+	            	<div>
+	            		<p> INFO DE LAS SEDES </p>
+	            	</div>
+	            </div>
+
+			</section>
+
+	</header>
+	
+</body>
+</html>
