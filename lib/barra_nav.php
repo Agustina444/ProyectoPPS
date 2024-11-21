79b37665bee2aadd566d4f3dd9f1918488efd850
Agustina444 <alanosa4@gmail.com>
2024-11-21 09:03:48 -0300

    actualizacion_barra_nav

--- lib/barra_nav.php
+++ lib/barra_nav.php
@@ -1,20 +1,57 @@
-<?php session_start(); ?>
-<link rel="stylesheet" href="/static/css/barra_nav.css">
-<nav>
-    <ul>
-        <li><a href="/index.php">INICIO</a></li>
-        <li><a href="/sede.php">SEDE</a></li>
-        <li><a href="/contacto.php">CONTACTO</a></li>
-        <li><a href="/clases/lista.php">CLASES</a></li>
-        <li><a href="/tienda/productos.php">TIENDA</a></li>
-        <?php if (isset($_SESSION['logueado']) && $_SESSION['logueado']) { ?>
-            <?php if ($_SESSION['categoria'] == 1) { // es admin ?>
-                <li><a href="/admin/administrador.php">ADMIN</a></li>
+<?php 
+// Comienza la sesión si no esta creada
+if(!isset($_SESSION)) session_start();
+?>
+
+<style>
+    /* Personaliza el color de fondo del dropdown cuando tiene el mouse encima */
+    .dropdown-item:hover {
+        background-color: #23262a; /* Cambia a un color oscuro */
+        color: white;
+    }
+</style>
+<nav class="navbar navbar-expand-sm navbar-dark" style="background-color: black;">
+    <a class="navbar-brand ml-1 mb-0 h1" href="/index.php">LEMA Fit</a>
+    <!-- Menu hamburgesa -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Items -->
+    <div class="collapse navbar-collapse mr-1" id="navbarSupportedContent">
+        <div class="navbar-nav ml-auto">
+            <a class="nav-link text-light" href="/index.php">Inicio</a>
+            <a class="nav-link text-light" href="/contacto.php">Contacto</a>
+            <a class="nav-link text-light" href="/clases/lista.php">Clases</a>
+            <a class="nav-link text-light" href="/tienda/productos.php">Tienda</a>
+            <!-- Si el usuario esta logueado -->
+            <?php if (isset($_SESSION['logueado']) && $_SESSION['logueado']) { ?>
+                <!-- Si el usuario es admin -->
+                <?php if (isset($_SESSION['categoria']) && $_SESSION['categoria'] == 1) { ?>
+                    <a class="nav-link text-light" href="/admin/administrador.php">Admin</a>
+                <?php } ?>
+                <!-- Muestra el dropdown de usuario -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="false">
+                        <?php echo $_SESSION['nombre']; ?>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right bg-dark">
+                        <a class="dropdown-item text-light" href="/usuario/perfil.php">Perfil</a>
+                        <a class="dropdown-item text-light" href="/lib/cerrar_sesion.php">Cerrar Sesión</a>
+                    </div>
+                </li>
+            <!-- Si el usuario no inicio sesion -->
+            <?php } else { ?>
+                <!-- Muestra el dropdown de invitado-->
+                <li class="nav-item dropdown">
+                    <a class="nav-link text-light dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Invitado
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right bg-dark">
+                        <a class="dropdown-item text-light" href="/usuario/login.php">Login</a>
+                        <a class="dropdown-item text-light" href="/usuario/registro.php">Registro</a>
+                    </div>
+                </li>
             <?php } ?>
-            <li><a href="/lib/cerrar_sesion.php">CERRAR SESIÓN</a></li>
-        <?php } else { ?>
-            <li><a href="/usuario/login.php">INGRESAR</a></li>
-            <li><a href="/usuario/registro.php">REGISTRO</a></li>
-        <?php } ?>
-    </ul>
+        </div>
+    </div>
 </nav>
