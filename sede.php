c244c94f1a18a2d02e784a1daed2e9ce272524bc
Emiliano <emiliano0montes@gmail.com>
2024-11-26 23:45:25 -0300

    Seccion Sede

--- sede.php
+++ sede.php
@@ -14,18 +14,82 @@
 		<?php include 'lib/barra_nav.php'; ?> 
 	</header>
 
-	<section id="sedes" class="sedes row">
-		<h3 class="col"> Gym Moron </h3>
+	<section id="sedes" class="sedes row g-0">
 
-		<div class="col">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.7083152168643!2d-58.63916912395666!3d-34.68731047292443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc728e545f8d7%3A0xa3b92b7c2044a4e1!2sAv.%20Eva%20Per%C3%B3n%203457%2C%20B1712%20Mor%C3%B3n%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1732475963139!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>			
+		<div class="row-12 sede-banner">
+			<h1> Gym Moron </h1>	
 		</div>
+
 		
-		<div class="col">
-			<p> Lunes a Viernes: 7 a 22hs  </p>
-			<p> Sabados: 9 a 18hs </p>
-			<p> Domingos/Feriados: Cerrado </p>
+		<div class="col-6">
+			<div class="row-12 d-flex">
+				<div class="col p-2">
+					<div class="row">
+						<img class="col-3 sede-icon" src="/static/svg/marker.svg" alt="">
+						<div class="col-9">
+							<h3>Direccion</h3>
+						</div>
+						
+					</div>
+				</div>
+	
+				<div class="col p-2">
+					<div class="row">
+						<img class="col-3 sede-icon" src="/static/svg/watch.svg" alt="">
+						<div class="col-9">
+							<h3>Horarios</h3>
+							<h4> Lunes a Viernes: 7 a 22hs  </h4>
+							<h4> Domingos/Feriados: Cerrado </h4>
+						</div>
+						
+					</div>
+				</div>		
+			</div>
+
+			<div class="row-12 d-flex">
+				<div class="col p-2">
+					<div class="row">
+						<img class="col-3 sede-icon" src="/static/svg/mail.svg" alt="">
+						<div class="col-9">
+							<h3>Mail</h3>
+							<h4>gymmoron@gmail.com</h4>
+						</div>
+						
+					</div>
+
+				</div>
+	
+				<div class="col p-2">
+					<div class="row">
+						<img class="col-3 sede-icon" src="/static/svg/phone.svg" alt="">
+						<div class="col-9">
+							<h3>Telefono</h3>
+							<h4>4657-5543</h4>
+						</div>
+					</div>
+				</div>				
+			</div>			
+
+			<div class="row-12 d-flex">
+				<div class="col p-2">
+					<div class="row">
+						<img class="col-2 sede-icon" src="/static/svg/weight.svg" alt="">
+						<div class="col-10">
+							<h3>Equipamiento</h3>
+							<h4>Pesas</h4>
+							<h4>Maquinas</h4>
+							<h4>Colchonetas</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 		</div>
+
+		<div class="col-6 p-2">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.7083152168643!2d-58.63916912395666!3d-34.68731047292443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc728e545f8d7%3A0xa3b92b7c2044a4e1!2sAv.%20Eva%20Per%C3%B3n%203457%2C%20B1712%20Mor%C3%B3n%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1732475963139!5m2!1ses-419!2sar" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>			
+		</div>
+
 	</section>
 
 </body>
